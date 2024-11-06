baf4b1ad699e564861f551369a41de29ca858245
硕 <202111580835@stu.blcu.edu.cn>
2024-11-06 19:42:57 +0800

    73-94
    73-94

---
+++ Lecture 73-94/glframework/shader.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef SHADER
+#define SHADER
+#include"core.h"
+class Shader {
+public:
+	Shader(const char* vertexPath, const char* fragmentPath);
+	~Shader();
+
+	void begin();//开始使用当前的Shader，用于切换绑定的Shader（状态机）
+	void end();//结束使用当前的Shader，用于切换绑定的Shader（状态机）
+	void setFloat(const std::string& name, float value);
+	void setVector3(const std::string& name, float x, float y, float z);
+	void setVector3(const std::string& name, const float* values);
+	void setInt(const std::string& name, int value);
+	void setMatrix4x4(const std::string& name, glm::mat4 value);
+public:
+	GLuint mProgram{ 0 };
+
+private:
+	//shader program, type: COMPILE / LINK
+	void checkShaderErrors(GLuint target, std::string type);
+};
+#endif
---
+++ Lecture 73-94/main.cpp
@@ -0,0 +1,189 @@
+#include <iostream>
+#include"glframework/core.h"
+#include"glframework/shader.h"
+#include <assert.h>
+#include "wrapper/checkError.h"
+#include "application/Application.h"
+#include "glframework/texture.h"
+#pragma warning(disable:4244)
+GLuint vao, program ,ebo; 
+Shader* shader = nullptr;
+Texture* texture = nullptr;
+Texture* texture2 = nullptr;
+glm::mat4 transform(1.0f);
+glm::mat4 viewMatrix(1.0f);
+glm::mat4 orthoMatrix(1.0f);
+glm::mat4 perspectiveMatrix(1.0f);
+
+float angle = 0.0f;
+void OnResize(int width, int height) {
+	std::cout << "窗体最新大小" << width << "，" << height << std::endl;
+	GL_CALL(glViewport(0, 0, width, height));
+}
+void OnKey(int key, int action, int mods) {
+	std::cout << key << std::endl;
+}
+void doRotation() {
+	//angle += 2.0f;
+	//transform = glm::rotate(glm::mat4(1.0f), glm::radians(angle), glm::vec3(0.0f, 0.0f, 1.0f));
+	angle = 1.0f;
+	transform = glm::rotate(transform, glm::radians(angle), glm::vec3(0.0f, 0.0f, 1.0f));
+}
+void doRotationTransform() {
+	transform = glm::rotate(glm::mat4(1.0f), glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+}
+void doTranslateTransform() {
+	transform = glm::translate(glm::mat4(1.0f), glm::vec3(0.5f, 0.0f, 0.0f));
+}
+void doScaleTransform() {
+	transform = glm::scale(glm::mat4(1.0f), glm::vec3(0.5f, 0.5f, 1.0f));
+}
+void doTransform() {
+	glm::mat4 rotateMat = glm::rotate(glm::mat4(1.0f), glm::radians(45.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+	glm::mat4 translateMat = glm::translate(glm::mat4(1.0f), glm::vec3(0.5f, 0.0f, 0.0f));
+	transform = rotateMat * translateMat;
+}
+void prepareInterleavedBuffer() {
+	//float vertices[] = {
+	//	-0.5f,-0.5f,0.0f,1.0f,0.0f,0.0f,0.0f,0.0f,
+	//	0.5f,-0.5f,0.0f,0.0f,1.0f,0.0f,1.0f,0.0f,
+	//	0.0f,0.5f,0.0f,0.0f,0.0f,1.0f,0.5f,1.0f
+	//};
+	//float vertices[] = {
+	//-0.5f,-0.5f,0.0f,1.0f,0.0f,0.0f,0.0f,0.0f,
+	//0.5f,-0.5f,0.0f,0.0f,1.0f,0.0f,10.0f,0.0f,
+	//0.0f,0.5f,0.0f,0.0f,0.0f,1.0f,5,10
+	//};
+	float vertices[] = {
+-1.0f,0.0f,0.0f,1.0f,0.0f,0.0f,0.0f,0.0f,
+1.0f,0.0f,0.0f,0.0f,1.0f,0.0f,1.0f,0.0f,
+0.0f,1.0f,0.0f,0.0f,0.0f,1.0f,0.5f,1.0f
+	};
+	unsigned int indices[] = {
+		0,1,2,
+	};
+	GLuint vbo = 0;
+	glGenBuffers(1, &vbo);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo); //绑定
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);//传数据
+
+	glGenBuffers(1, &ebo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	//GLuint vao = 0;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	//动态获取位置
+	GLuint posLocation = glGetAttribLocation(shader->mProgram, "aPos");
+	GLuint colorLocation = glGetAttribLocation(shader->mProgram, "aColor");
+	GLuint uvLocation = glGetAttribLocation(shader->mProgram, "aUV");
+	//动态获取Uniform编号
+	// GLuint location = glGetUniformLocation(program,"time"); 
+	// 设置变量的值
+	// glUniform1f(location,0.5f);
+
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+	glEnableVertexAttribArray(posLocation);
+	//索引，该属性几个数字，类型，是否归一化，每个顶点大小，该属性在一个顶点的偏移量
+	glVertexAttribPointer(posLocation, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+
+	glEnableVertexAttribArray(colorLocation);//绑定的索引
+	//索引，该属性几个数字，类型，是否归一化，每个顶点大小，该属性在一个顶点的偏移量
+	glVertexAttribPointer(colorLocation, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+
+	glEnableVertexAttribArray(uvLocation);//绑定的索引
+	//索引，该属性几个数字，类型，是否归一化，每个顶点大小，该属性在一个顶点的偏移量
+	glVertexAttribPointer(uvLocation, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6* sizeof(float)));
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);//把ebo绑给vao
+
+	glBindVertexArray(0); //解绑VAO
+}
+
+void prepareShader() {
+	shader = new Shader("assets/shaders/vertex.glsl", "assets/shaders/fragment.glsl");
+}
+
+void prepareTexture() {
+	texture = new Texture("assets/textures/std-image.jpg", 0);
+	texture2 = new Texture("assets/textures/wf.jpg", 1);
+}
+
+void prepareCamera() {
+	//lookat生成viewMatrix，eye当前摄像机所在位置，center当前摄像机看向的点，相机的向上方向
+	viewMatrix = glm::lookAt(glm::vec3(3.0f,0.0f,1.0f),glm::vec3(0.0f,0.0f,0.0f),glm::vec3(0.0f,1.0f,0.0f));
+}
+void prepareOrtho() {
+	orthoMatrix = glm::ortho(-2.0f, 2.0f, -2.0f, 2.0f, 2.0f, -2.0f);//左右 下上 近远
+}
+void preparePerspective() {
+	//FOV 上下视张角，aspect：近平面横纵比，近平面距离，原平面距离
+	perspectiveMatrix = glm::perspective(glm::radians(60.0f), (float)app->getWidth()/(float)app->getHeight(), 0.1f, 1000.0f);
+}
+void render() {
+	GL_CALL(glClear(GL_COLOR_BUFFER_BIT));//执行画布清理操作, GL_CALL用来错误检查的宏定义
+
+	//绑定shader program
+	//glUseProgram(program);
+	shader->begin();
+
+	//glGetUniformLocation必须在UseProgram后面使用，也就是shader->begin后使用
+	float color[] = { 0.5f,0.3f,0.4f };
+	shader->setVector3("uColor", color);
+	shader->setFloat("time", glfwGetTime());//传的变量在VS FS中声明后都可以使用，因为vs fs已经合成为了program
+	shader->setInt("sampler", 0);
+	shader->setInt("sampler", 1);
+	shader->setFloat("width", texture->getWidth());
+	shader->setFloat("height", texture->getHeight());
+	shader->setMatrix4x4("transform", transform);
+	shader->setMatrix4x4("viewMatrix", viewMatrix);
+	shader->setMatrix4x4("orthoMatrix", orthoMatrix);
+	shader->setMatrix4x4("perspectiveMatrix", perspectiveMatrix);
+	glBindVertexArray(vao);
+	//glDrawArrays(GL_TRIANGLES,0,3);//GL_TRIANGLES_STRIP/ FAN
+
+	//modes , 几个点，类型，0/偏移/索引数组
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0); //解绑VAO
+
+	//解除绑定Shader，下一个三角形要换绑
+	shader->end();
+}
+
+
+int main() {
+	if (!app->init(800, 600)) {
+		return -1;
+	}
+	////设置监听，监听窗体大小变化的消息
+	app->setResizeCallback(OnResize);
+
+	////设置监听键盘事件
+	app->setKeyBoardCallback(OnKey);
+
+	//设置视口大小和清理颜色
+	GL_CALL(glViewport(0, 0, 800, 600));
+	GL_CALL(glClearColor(0.2f, 0.3f, 0.3f, 1.0f));
+
+	prepareShader();
+	prepareInterleavedBuffer();
+	prepareTexture(); 
+	prepareCamera();
+	prepareOrtho();
+	preparePerspective();
+	//渲染循环
+	while (app->update()) {
+	
+		//doRotation();
+		render();
+	}
+
+	app->destory();
+	delete texture;
+	return 0;
+}
